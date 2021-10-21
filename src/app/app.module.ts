ffe941454fb712d9536ae935382986712f6233f0
Panupong Khongsanae <zakam141@gmail.com>
2021-10-21 14:33:06 +0700

    add project angular my self

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent,
+    AboutusComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
